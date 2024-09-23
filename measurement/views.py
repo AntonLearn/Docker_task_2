f4d7d91fc17ed839e9dc1e4ba967693790363c74
osboxes.org <osboxes@osboxes.org>
2024-09-23 11:06:48 -0400

    Docker_task_2

---
+++ measurement/views.py
@@ -0,0 +1,43 @@
+from rest_framework.generics import RetrieveUpdateAPIView
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+
+from measurement.models import Sensor, Measurement
+from measurement.serializers import CreateMeasurementSerializer
+from measurement.serializers import SensorCreateSerializer
+from measurement.serializers import ShortListSensorsSerializer
+from measurement.serializers import SensorUpdateRetrieveSerializer
+
+
+class SensorCreate(ListCreateAPIView):
+    """Создать датчик. Указываются название и описание датчика."""
+    queryset = Sensor.objects.all()
+    serializer_class = SensorCreateSerializer
+
+
+class ShortListSensors(ListCreateAPIView):
+    """Получить список датчиков. Выдается список с краткой информацией по датчикам:
+    ID, название и описание"""
+    queryset = Sensor.objects.all()
+    serializer_class = ShortListSensorsSerializer
+
+
+class SensorUpdateRetrieve(RetrieveUpdateAPIView):
+    """Получить информацию по конкретному датчику.
+    ID, название, описание и список всех измерений с температурой и временем"""
+    queryset = Sensor.objects.all()
+    serializer_class = SensorUpdateRetrieveSerializer
+
+
+class MeasurementCreate(CreateAPIView):
+    """Добавить измерение. Указываются ID датчика и температура"""
+    queryset = Measurement.objects.all()
+    serializer_class = CreateMeasurementSerializer
+
+
+class DetailListSensors(ListCreateAPIView):
+    """Получить список датчиков. Выдается список с полной информацией по датчикам:
+    ID, название, описание, измерения"""
+    queryset = Sensor.objects.all()
+    serializer_class = SensorUpdateRetrieveSerializer
